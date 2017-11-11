e4ee0308ed3212db33a2d332865aab6bcfc440db
Pedro Nauck <pedronauck@gmail.com>
2017-11-11 02:41:43 -0200

    chore: update yargs

--- bin/lib.js
+++ bin/lib.js
@@ -8,35 +8,37 @@ const ROOT_PATH = fs.realpathSync(process.cwd())
 const DEFAULT_CONFIG_FILE = `${ROOT_PATH}/lib.config.js`
 const DEFAULT_TSCONFIG = `${ROOT_PATH}/tsconfig.json`
 
-const args = {
-  'config': {
-    alias: 'c',
-    default: DEFAULT_CONFIG_FILE,
-  },
-  'extensions': {
-    alias: 'ex',
-    default: ['.js', '.jsx'],
-  },
-  'tsconfig': {
-    default: DEFAULT_TSCONFIG,
-  },
-  'gzip': {
-    default: false,
-  },
-  'production': {
-    alias: 'p',
-    default: false,
-  },
-  'watch': {
-    alias: 'w',
-    default: false,
-  },
-}
-
 yargs
   .usage('$0 <cmd> [args]')
-  .command('build', 'build your library', args, () => {
-    require('../scripts/build')
+  .command({
+    command: 'build [args]',
+    builder: ({ positional }) => {
+      positional('config', {
+        alias: ['c', 'cfg'],
+        default: DEFAULT_CONFIG_FILE,
+      })
+      positional('extensions', {
+        alias: 'ext',
+        default: ['.js', '.jsx'],
+      })
+      positional('tsconfig', {
+        default: DEFAULT_TSCONFIG,
+      })
+      positional('gzip', {
+        default: false,
+      })
+      positional('production', {
+        alias: 'p',
+        default: false,
+      })
+      positional('watch', {
+        alias: 'w',
+        default: false,
+      })
+    },
+    handler: () => {
+      require('../scripts/build')
+    }
   })
   .help()
   .argv
